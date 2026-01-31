3b62f8d1ec1539a1b49701f5498a3d8a37e8d6e5
Emmanuel Kabanga <hopekab@gmail.com>
2026-01-31 18:35:35 +0900

    Clean up __init__.py metadata

--- src/__init__.py
+++ src/__init__.py
@@ -5,10 +5,6 @@ This package provides implementations of three generative models:
 - GAN (Generative Adversarial Network)
 - VAE (Variational Autoencoder)
 - Diffusion Model with frequency-guided blending
-
-For more information, see the README.md file.
 """
 
 __version__ = "1.0.0"
-__author__ = "Your Name"
-__email__ = "your.email@example.com"
